c6cf6491774b1ed83d4ec5a6ac0ac540047b7a1d
darkrage <darkrage6162@gmail.com>
2021-08-29 11:13:04 -0700

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+let colors = [
+  '96C672',
+  '00AFF0',
+  '6878F6',
+  'B21FF7',
+
+  '474EB4',
+  'E03E48',
+  '9B55A9',
+  'F77E4A',
+  '27B26F',
+  'EDC537',
+  '3E5BB7',
+  '1C1917',
+  '11362A',
+  'F03630',
+
+  '278DC2',
+  'AB253B',
+  'AFC271',
+  '3B576D',
+  'E9792F',
+  '14A800',
+  '00BFB5',
+  '00A99F',
+  '4E5CF0',
+  'EF5A4D',
+
+];
+function colorScan() {
+  for (var i = 0; i < colors.length; i++) {
+    let newColor = document.createElement('content');
+    //newColor.classList.add('work');
+    newColor.style.background = '#'+colors[i];
+    document.querySelector('main').appendChild(newColor);
+
+  }
+}
+
+colorScan();
