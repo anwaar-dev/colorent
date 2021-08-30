ca4bf9ad14935030fb2ebb87210d286ea65852d4
darkrage <darkrage6162@gmail.com>
2021-08-30 12:55:15 -0700

    like colors...design

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+let starterData = {
+  Liked:[],
+};
+
+
+
+function Login() {
+  name = "ColorentData";
+  if (localStorage.getItem(name)==undefined){
+  jsonData = JSON.stringify(starterData);
+  localStorage.setItem(name, jsonData);
+  }
+  else{
+  let acc = name;
+  localStorage.setItem("acc",acc)
+  }
+}Login();
+// GETTING THE OLD DATA FROM LOCAL STORAGE
+let account,Data,accountData,saveAccountData;getData();
+function getData() {
+  account=localStorage.getItem("acc");
+  accountData=localStorage.getItem(account);
+  Data=JSON.parse(accountData);
+}
+function save() {
+  saveAccountData=JSON.stringify(Data);
+  localStorage.setItem(account,saveAccountData);
+}
+// ASIGNING THE RETRIVED DATA
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let colors = [
   '96C672',
   '00AFF0',
@@ -28,14 +79,59 @@ let colors = [
   '17A05D',
 
 ];
+
+
+
+
+
+
 function colorScan() {
   for (var i = 0; i < colors.length; i++) {
     let newColor = document.createElement('content');
-    newColor.innerHTML='<h2>#'+colors[i]+'</h2>';
+    newColor.innerHTML='<a class="likeButt" onclick="Like('+i+');"></a>'+'<h2>#'+colors[i]+'</h2>';
     newColor.style.background = '#'+colors[i];
     document.querySelector('main').appendChild(newColor);
-
   }
 }
-
+function likedColorScan() {
+  for (var i = 0; i < Data.Liked.length; i++) {
+    let newLikeColor = document.createElement('content');
+    newLikeColor.innerHTML='<a class="disLikeButt" onclick="Dislike('+i+');"></a>'+'<h2>#'+Data.Liked[i]+'</h2>';
+    newLikeColor.style.background = '#'+Data.Liked[i];
+    document.querySelector('#liked').appendChild(newLikeColor);
+  }
+}
 colorScan();
+likedColorScan();
+
+
+
+
+
+
+
+
+function Like(c) {
+  Data.Liked.push(colors[c]);
+
+  let uniqueSet = new Set(Data.Liked);
+// Set { '🐑', 1, 2, 3 }
+
+// Step 2
+  Data.Liked = [...uniqueSet];
+
+  document.querySelector('#liked').innerHTML='';
+  likedColorScan()
+  save();
+}
+function Dislike(c) {
+  Data.Liked.splice(c,1);
+  save();
+}
+
+
+
+
+
+
+
